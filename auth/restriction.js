cfb95785be22a47bfed2d0d908889978da9e56ca
Erica L. Ingram <inquiries@aquoco.co>
2020-08-17 20:38:00 -0700

    added login/register routes

--- auth/restriction.js
+++ auth/restriction.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   const token = req.headers.authorization;
 
   if (token) {
-    const secret = process.env.JWT_SECRET || "Hatchways BE Test Secret";
+    const secret = process.env.JWT_SECRET;
 
     jwt.verify(token, secret, (err, decodedToken) => {
       if (err) {
