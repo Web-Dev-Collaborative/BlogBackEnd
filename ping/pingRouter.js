bad04ab96455cf8912ff55f6796130d041c75025
Erica L. Ingram <inquiries@aquoco.co>
2020-08-18 05:04:57 -0700

    fixed ping endpoint 3

--- ping/pingRouter.js
+++ ping/pingRouter.js
@@ -4,9 +4,12 @@ const Ping = require('./pingModel.js');
 
 // GET:  ping server
 router.get('/', (req, res) => {
+	let ping = {
+		"success": true
+	};
 	Ping.find()
 		.then(authors => {
-			res.status(200).json({"success": true});
+			res.status(200).json(ping);
 		})
 		.catch(err => res.send(err));
 });
