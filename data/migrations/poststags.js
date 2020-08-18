e439acf5bd15e0336eaa284e40408c1620b51862
Erica L. Ingram <inquiries@aquoco.co>
2020-08-18 01:33:33 -0700

    more migration fixes

--- data/migrations/poststags.js
+++ data/migrations/poststags.js
@@ -1,25 +1,25 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('poststags', poststags => {
-  
-      poststags.increments('poststagsid');
-  
-      poststags
-      .integer('postsid')
-      .unsigned()
-      .notNullable()
-      .references('postsid').inTable('posts')
-      .onUpdate('CASCADE').onDelete('RESTRICT');
-  
-      poststags 
-      .integer('tagsid')
-      .unsigned()
-      .notNullable()
-      .references('tagsid').inTable('tags')
-      .onUpdate('CASCADE').onDelete('RESTRICT');
+  return knex.schema.createTable('poststags', poststags => {
 
-    });
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('poststags');
-  }
+    poststags.increments('poststagsid');
+
+    poststags
+    .integer('postsid')
+    .unsigned()
+    .notNullable()
+    .references('postsid').inTable('posts')
+    .onUpdate('CASCADE').onDelete('RESTRICT');
+
+    poststags 
+    .integer('tagsid')
+    .unsigned()
+    .notNullable()
+    .references('tagsid').inTable('tags')
+    .onUpdate('CASCADE').onDelete('RESTRICT');
+
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('poststags');
+}
