7a41bc5883887181f0700a91ca96717f58d78b2c
Erica L. Ingram <inquiries@aquoco.co>
2020-08-18 23:40:20 -0700

    fixing tags query parameter

--- posts/postsHelpers.js
+++ posts/postsHelpers.js
@@ -3,9 +3,10 @@ module.exports = {
     filterResults
   };
   
+function isTagsFieldArray(tagsField){return Array.isArray(tagsField);};
 
 function validateTag(tagsField){
-    const isArray = Array.isArray(tagsField);
+    const isArray = isTagsFieldArray(tagsField);
 	// validate tagsField
 		// available tags:  culture, design, health, history, politics, science, startups, tech
 	// if tagsField is array or not
@@ -15,30 +16,30 @@ function validateTag(tagsField){
             // else return true
 	if(isArray === true){
 		tagsField.forEach((tag)=>{
-			if(tag !== 'culture' || 
-			   tag !== 'design' || 
-			   tag !== 'health' || 
-			   tag !== 'history' || 
-			   tag !== 'politics' || 
-			   tag !== 'science' || 
-			   tag !== 'startups' || 
-			   tag !== 'tech'){
-                   // if not one of valid tags, return false
-                   return false;
+			if(tag === 'culture' || 
+			   tag === 'design' || 
+			   tag === 'health' || 
+			   tag === 'history' || 
+			   tag === 'politics' || 
+			   tag === 'science' || 
+			   tag === 'startups' || 
+			   tag === 'tech'){
+                   // if one of valid tags, return true
+                   return true;
 			   }
 		});
-        // else return true
+        // else return false
         return true;
 	}
-		// if tagsField is not array
-		if(tagsField === 'culture' || tagsField === 'design' || tagsField === 'health' || 
-		   tagsField === 'history' || tagsField === 'politics' || tagsField === 'science' || 
-		   tagsField === 'startups' || tagsField === 'tech'){
-            // if tag is invalid, return false
-            return true;
-		}
-        // else return true
-        return false;
+    // if tagsField is not array
+    if(tagsField === 'culture' || tagsField === 'design' || tagsField === 'health' || 
+        tagsField === 'history' || tagsField === 'politics' || tagsField === 'science' || 
+        tagsField === 'startups' || tagsField === 'tech'){
+        // if tag is valid, return false
+        return true;
+    }
+    // else return false
+    return false;
 }
 
 // filter results by tagsField
