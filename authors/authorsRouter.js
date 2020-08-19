b45c31fb4860c0630c0ca81586fc3ea75a1ed7a1
Erica L. Ingram <inquiries@aquoco.co>
2020-08-19 14:44:16 -0700

    fixing initial all-authors endpoint

--- authors/authorsRouter.js
+++ authors/authorsRouter.js
@@ -33,8 +33,43 @@ router.get('/', restricted, (req, res) => {
 					error: err
 				});
 			} else {
-				// authors, posts, tagsPerAuthor, likesPerAuthor, readsPerAuthor
-				res.status(200).json(authors);
+				Authors.getPostsByAllAuthors()
+					.then(posts => {
+						if (!posts) {
+							res.status(404).json({
+								message: `Posts do not exist.`,
+								error: err
+							});
+						} else {
+							Authors.getTagsByAllAuthors()
+								if (!tagsPerAuthor) {
+									res.status(404).json({
+										message: `Tags do not exist.`,
+										error: err
+									});
+								} else {
+									Authors.getAllTotalLikesCount()
+										if (!likesPerAuthor) {
+											res.status(404).json({
+												message: `Total likes count does not exist.`,
+												error: err
+											});
+										} else {
+											Authors.getAllTotalReadsCount()
+											if (!readsPerAuthor) {
+												res.status(404).json({
+													message: `Total reads count does not exist.`,
+													error: err
+												});
+											} else {
+												// authors, posts, tagsPerAuthor, likesPerAuthor, readsPerAuthor
+												res.status(200).json(authors);
+											}
+										}
+								}
+						}
+					})
+					.catch(err => res.send(err));
 			}
 		})
 		.catch(err => res.send(err));
