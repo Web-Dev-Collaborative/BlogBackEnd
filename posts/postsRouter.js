0621002b42234ada7ee2c9abdc5dc7ae6743687b
Erica L. Ingram <inquiries@aquoco.co>
2020-08-19 04:14:11 -0700

    adding array functionality to tags qm functionality

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -53,13 +53,9 @@ router.get('/', restricted, (req, res) => {
 				if(isArray === true){
 					// if IS valid tag, run filterResults on response and return it
 					if(isValidTag === true){
-						filteredResults = posts.posts.filter(post => {
+						filteredResults = posts.filter(post => {
 							for(let x = 0; x < tagsField.length; x++){
-								if (post.tags.includes(tagsField[x]) === true) {
-									return true;
-								} else if (post.tags.includes(tagsField[x]) === false){
-									return false;
-								}
+								post.tags.includes(tagsField[x]) === true;
 							}
 						});
 						res.status(200).json({posts: filteredResults});
