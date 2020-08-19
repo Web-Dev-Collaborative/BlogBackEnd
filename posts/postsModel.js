a0edb240a1093626c768b9207800e938d239d464
Erica L. Ingram <inquiries@aquoco.co>
2020-08-18 21:54:36 -0700

    fixing all-posts router

--- posts/postsModel.js
+++ posts/postsModel.js
@@ -37,7 +37,18 @@ function getPosts() {
 	.innerJoin('authors', 'posts.authorsid', 'authors.authorsid');
 	
 		*/
-	return db('posts').raw('SELECT authors.firstname AS firstname, authors.lastname AS lastname, posts.postsid as id, posts.authorsid as authorId, posts.likes as likes, posts.reads as reads, ARRAY_AGG(tags.tagname) AS tags FROM posts INNER JOIN authors ON posts.authorsid = authors.authorsid INNER JOIN poststags ON posts.postsid = poststags.postsid INNER JOIN tags ON poststags.tagsid = tags.tagsid GROUP BY posts.postsid, posts.authorsid, authors.firstname, authors.lastname, posts.likes, posts.reads;');
+	return db('posts')
+		.select('authors.firstname AS firstname', 'authors.lastname AS lastname',
+			'posts.authorsid AS authorId',
+			'posts.postsid AS id', 'posts.likes AS likes', 'posts.reads AS reads',
+			db.raw('ARRAY_AGG(tags.tagname) AS tags')
+		)
+		.innerJoin('authors', 'posts.authorsid', 'authors.authorsid')
+		.innerJoin('poststags', 'posts.postsid', 'poststags.postsid')
+		.innerJoin('tags', 'poststags.tagsid', 'tags.tagsid')
+		.groupBy('posts.postsid', 'posts.authorsid', 
+		'authors.firstname', 'authors.lastname',
+		'posts.likes', 'posts.reads');
 		/*
 		.select('authors.firstname AS firstname', 'authors.lastname AS lastname',
 			'posts.authorsid AS authorId',
