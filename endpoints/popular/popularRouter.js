34e8cc73127ae0f34ff979904119a14dadae2d04
Erica L. Ingram <inquiries@aquoco.co>
2020-08-21 04:39:22 -0700

    fixed most read/liked endpoints routers

--- endpoints/popular/popularRouter.js
+++ endpoints/popular/popularRouter.js
@@ -13,7 +13,6 @@ const { cache } = require("../../cache/cacheHelpers.js");
 
 // GET:  gets posts list in order of most to least liked
 router.get("/mostliked", restricted, cache(10), (req, res) => {
-	const tagsField = req.query.tags;
 	Posts.getPosts()
 		.then((posts) => {
 			if (!posts) {
@@ -22,51 +21,17 @@ router.get("/mostliked", restricted, cache(10), (req, res) => {
 					error: err
 				});
 			} else {
-				let filteredResults, newTagsField;
-				if (tagsField.includes(',')) {newTagsField = tagsField.split(',')}
-				else {newTagsField = [tagsField]};
-				let isTFArray = isTagsFieldArray(newTagsField);
-				let isValidTag = validateTag(newTagsField);
-
-				// if multiple tags
-				if (isTFArray === true) {
-					// if IS valid tag, run filterResults on response and return it
-					if (isValidTag === true) {
-						filteredResults = posts.filter(post => {
-							return newTagsField.every(tag => post.tags.includes(tag));
-						});
-						res.status(200).json({ posts: filteredResults });
-					} else if (isValidTag === false) {
-						// if IS NOT valid tag, return error response
-						res.status(400).json({ error: 'Tags parameter is invalid.' });
-					};
-				};
-				// if single tag
-				if (isTFArray === false) {
-					if (isValidTag === true) {
-						// if IS valid tag, return filtered results
-						filteredResults = posts.filter(post => {
-							return post.tags.indexOf(newTagsField) >= 0;
-						});
-						// sorting posts by most to least likes
-						filteredResults = filteredResults.sort((a, b) => (a['likes'] > b['likes'] ? -1 : 1));
 						
-						res.status(200).json({ posts: filteredResults });
-					} else if (isValidTag === false) {
-						
-						// sorting posts by most to least likes
-						filteredResults = filteredResults.sort((a, b) => (a['likes'] > b['likes'] ? -1 : 1));
-						res.status(200).json({ posts: filteredResults });
-					};
-				};
-			};
+				// sorting posts by most to least likes
+				posts = posts.sort((a, b) => (a['likes'] > b['likes'] ? -1 : 1));
+				res.status(200).json({ posts: posts });
+			}
 		})
 		.catch(err => res.send(err));
 });
 
 // GET:  gets posts list in order of most to least read
 router.get("/mostread", restricted, cache(10), (req, res) => {
-	const tagsField = req.query.tags;
 	Posts.getPosts()
 		.then((posts) => {
 			if (!posts) {
@@ -75,57 +40,13 @@ router.get("/mostread", restricted, cache(10), (req, res) => {
 					error: err
 				});
 			} else {
-				let filteredResults, newTagsField;
-				if (tagsField.includes(',')) {newTagsField = tagsField.split(',')}
-				else {newTagsField = [tagsField]};
-				let isTFArray = isTagsFieldArray(newTagsField);
-				let isValidTag = validateTag(newTagsField);
-
+						
 				// sorting posts by most to least reads
 				posts = posts.sort((a, b) => (a['reads'] > b['reads'] ? -1 : 1));
-
-				// validate sortField
-					// if sort criteria not valid
-						// available sorts:  author, authorId, id, likes, reads
-
-				// if multiple tags
-				if (isTFArray === true) {
-					// if IS valid tag, run filterResults on response and return it
-					if (isValidTag === true) {
-						filteredResults = posts.filter(post => {
-							return newTagsField.every(tag => post.tags.includes(tag));
-						});
-						
-						// sorting posts by most to least reads
-						filteredResults = filteredResults.sort((a, b) => (a['reads'] > b['reads'] ? -1 : 1));
-						
-						res.status(200).json({ posts: filteredResults });
-					} else if (isValidTag === false) {
-						// if IS NOT valid tag, return error response
-						res.status(400).json({ error: 'Tags parameter is invalid.' });
-					};
-				};
-				// if single tag
-				if (isTFArray === false) {
-					if (isValidTag === true) {
-						// if IS valid tag, return filtered results
-						filteredResults = posts.filter(post => {
-							return post.tags.indexOf(newTagsField) >= 0;
-						});
-
-						// sorting posts by most to least reads
-						filteredResults = filteredResults.sort((a, b) => (a['reads'] > b['reads'] ? -1 : 1));
-
-						res.status(200).json({ posts: filteredResults });
-					} else if (isValidTag === false) {
-						// if IS NOT valid tag, return error response
-						res.status(400).json({ error: 'Tags parameter is invalid.' });
-					};
-				};
-			};
+				res.status(200).json({ posts: posts });
+			}
 		})
 		.catch(err => res.send(err));
 });
-
 
 module.exports = router;
