fa7cdc685d18ab29dc305e085f74f74586265801
Erica L. Ingram <inquiries@aquoco.co>
2020-08-20 17:21:59 -0700

    added tags/authors and tags/posts to tags endpoint router

--- tags/tagsRouter.js
+++ tags/tagsRouter.js
@@ -152,15 +152,44 @@ router.get('/authors', restricted, cache(10), (req, res) => {
 });
 
 // GET:  gets posts per all tags
-    // /tags/posts
-	// chain getAllTags, getAllPostsByAllTags
 router.get('/posts', restricted, (req, res) => {
 	Tags.getAllTags()
 		.then(tags => {
 			Tags.getAllPostsByAllTags()
 				.then(postsByAllTags => {
 
-					res.status(200).json({tags: tags, posts: postsByAllTags});
+					let newTagsList = tags;
+					let currentTagName, currentPostsTagName;
+					let newTagsListLength = newTagsList.length;
+					let postsLength = postsByAllTags.length;
+					
+					// loop through newTagsList;
+						// add empty newTagsList[x].posts
+					for(let x = 0; x < newTagsListLength; x++){newTagsList[x].posts = [];}
+					
+					// loop through newTagsList;
+					// then loop through postsByAllTags
+					// if newTagsList[x].tagname === postsByAllTags[y].tagname
+						// add to newTagsList[x].posts
+					for(let x = 0; x < newTagsListLength; x++){
+						currentTagName = newTagsList[x].tagname;
+						for(let y = 0; y < postsLength; y++){
+							currentPostsTagName = postsByAllTags[y].tagname;
+							if(currentTagName === currentPostsTagName){
+								currentPost = 
+								{
+									"postsid": postsByAllTags[y].postsid,
+									"author": postsByAllTags[y].author,
+									"authorsid": postsByAllTags[y].authorsid,
+									"likes": postsByAllTags[y].likes,
+									"reads": postsByAllTags[y].reads
+								};
+								newTagsList[x].posts.push(currentPost);
+							}
+						}
+					
+					}
+					res.status(200).json(newTagsList);
 
 				})
 				.catch(err => res.send(err));
@@ -171,16 +200,145 @@ router.get('/posts', restricted, (req, res) => {
 // GET:  posts AND authors per all tags
 	// /tags
 	// chain getAllTags, getAllAuthorsByAllTags, getAllPostsByAllTags
-router.get('/', restricted, (req, res) => {
+router.get('/', restricted, (req, res) => {Tags.getAllTags()
+	.then(allTags => {
+		Tags.getAllAuthorsByAllTags()
+			.then(authorsByAllTags => {
+				Authors.getPostsByAllAuthors()
+					.then(postsByAllAuthors => {
+						Authors.getAllTotalLikesCount()
+						.then(allTotalLikesCounts => {
+							Authors.getAllTotalReadsCount()
+								.then(allTotalReadsCounts => {
+																		
+
+
+									let newTagsList = allTags;
+									let currentPost, currentTagName, postTagsLength;
+									let likesAuthorID, authorsAuthorID, totalLikesCount, currentAuthor;
+									let likesLength, authorsLength, totalReadsCount, readsLength;
+									let newTagsListLength, currentAuthorsTagName, postsLength;
+									let currentPostTagName, currentPostTags, currentPostAuthor;
+									// create new array as newTagsList = tags array
+									// sort authors, likes, reads & use y for all three
+									
+									allTotalLikesCounts.sort(compare1);
+									allTotalReadsCounts.sort(compare1);
+									// loop through likes and add to authors array
+									likesLength = allTotalLikesCounts.length;
+									authorsLength = authorsByAllTags.length;
+									readsLength = allTotalReadsCounts.length;
+									newTagsListLength = newTagsList.length;
+									postsLength = postsByAllAuthors.length;
+									for (let x = 0; x < likesLength; x++){
+										likesAuthorID = allTotalLikesCounts[x].authorsid;
+										totalLikesCount = allTotalLikesCounts[x].totallikecount;
+										for (let y = 0; y < authorsLength; y++){
+											authorsAuthorID = authorsByAllTags[y].id;
+											if(likesAuthorID === authorsAuthorID){
+												authorsByAllTags[y].totalLikesCount = totalLikesCount;
+											};
+										};
+									};
+									for (let u = 0; u < authorsLength;u++){
+										authorsByAllTags[u].posts = [];
+									}
+									
+									// loop through reads and add to authors array
+									for (let x = 0; x < readsLength; x++){
+										readsAuthorID = allTotalReadsCounts[x].authorsid;
+										totalReadsCount = allTotalReadsCounts[x].totalreadcount;
+										currentAuthorID = authorsByAllTags[x].id;
+										currentAuthorName = authorsByAllTags[x].author;
+										for (let y = 0; y < authorsLength; y++){
+											authorsAuthorID = authorsByAllTags[y].id;
+											if(readsAuthorID === authorsAuthorID){
+												authorsByAllTags[y].totalReadsCount = totalReadsCount;
+											};
+										};
+									};
+									for (let x = 0; x < newTagsListLength; x++){
+										newTagsList[x].authors = [];
+									}
+									
+									// loop through authors
+										// get tagname of each author
+									for (let x = 0; x < authorsLength; x++){
+										currentAuthorsTagName = authorsByAllTags[x].tagname;
+										currentAuthorName = authorsByAllTags[x].author;
+										// console.log('authors array info = ' + currentAuthorsTagName, currentAuthorName);
+									// loop through posts
+										// get tagnames of each post
+										for (let y = 0; y < postsLength; y++){
+											currentPost = postsByAllAuthors[y];
+											postTagsLength = postsByAllAuthors[y].tags.length;
+											currentPostTags = postsByAllAuthors[y].tags;
+											currentPostAuthor = postsByAllAuthors[y].author;
+											// console.log('posts array info = ' + currentPostTags, currentPostAuthor);
+											if(currentAuthorName === currentPostAuthor){
+											// if author of post matches author from authors array
+											// loop through each post's tags
+												// if author's tagname matches one of post's tagnames
+													// add that post to author's posts array
+												for (let z = 0; z < postTagsLength; z++){
+													currentPostTagName = currentPostTags[z];
+													if(currentPostTagName === currentAuthorsTagName){
+														authorsByAllTags[x].posts.push(currentPost);
+													}
+												}
+											}
+										}
+									}
+									// loop through tags
+									// loop through authors
+									// if authors.tagname == newTagsList.tagname, then add to newTagsList.authors
+									
+									for (let x = 0; x < newTagsListLength; x++){
+										currentTagName = newTagsList[x].tagname;
+										for (let y = 0; y < authorsLength; y++){
+											currentAuthorsTagName = authorsByAllTags[y].tagname;
+											if(currentTagName === currentAuthorsTagName){
+												// add authorsByAllTags[y] to newTagsList.authors[x]
+												currentAuthor = authorsByAllTags[y];
+												newTagsList[x].authors.push(currentAuthor);
+											}
+										}
+									}
+									// loop through newTagsList.authors
+										// add every property but newTagsList.authors.tagname
+									for (let x = 0; x < newTagsListLength; x++){
+										currentTag = newTagsList[x];
+										authorsLength = newTagsList[x].authors.length;
+										for (let y = 0; y < authorsLength; y++){
+											currentTag.authors[y] = {
+												bio: currentTag.authors[y].bio,
+												id: currentTag.authors[y].id,
+												author: currentTag.authors[y].author,
+												posts: currentTag.authors[y].posts,
+												totalLikesCount: currentTag.authors[y].totalLikesCount,
+												totalReadsCount: currentTag.authors[y].totalReadsCount
+											}
+										}
+									}
+							})
+							.catch(err => res.send(err));
+						})
+						.catch(err => res.send(err));
+
+					})
+					.catch(err => res.send(err));
+			})
+			.catch(err => res.send(err));
+	})
+	.catch(err => res.send(err));
+
 	Tags.getAllTags()
 		.then(tags => {
 			Tags.getAllPostsByAllTags()
 				.then(postsByAllTags => {
-
-					let newTagsList = tags;
-					let currentTagName, currentPostsTagName;
-					let newTagsListLength = newTagsList.length;
-					let postsLength = postsByAllTags.length;
+					newTagsList = tags;
+					newTagsListLength = newTagsList.length;
+					postsLength = postsByAllTags.length;
 					
 					// loop through newTagsList;
 						// add empty newTagsList[x].posts
