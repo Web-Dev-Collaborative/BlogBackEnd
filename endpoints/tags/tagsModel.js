8fcde1675249a5d3dca62b0598504e5960699904
Erica L. Ingram <inquiries@aquoco.co>
2020-08-21 01:44:25 -0700

    reformatted tags model

--- endpoints/tags/tagsModel.js
+++ endpoints/tags/tagsModel.js
@@ -1,4 +1,4 @@
-const db = require('../../data/dbConfig');
+const db = require("../../data/dbConfig");
 
 module.exports = {
 	add,
@@ -23,8 +23,8 @@ module.exports = {
 */
 // get tagsid & tagname on all tags
 function getAllTags(){
-	return db('tags')
-		.select('tags.tagsid', 'tags.tagname');
+	return db("tags")
+		.select("tags.tagsid", "tags.tagname");
 };
 
 /*
@@ -41,17 +41,17 @@ function getAllTags(){
 */
 // get all authors per all tags
 function getAllAuthorsByAllTags(){
-	return db('tags')
+	return db("tags")
 	.select(
-		'authors.bio as bio',
-		'authors.authorsid AS id', 
+		"authors.bio as bio",
+		"authors.authorsid AS id", 
 		db.raw("authors.firstname || ' ' || authors.lastname AS author"),
-		'tags.tagname AS tagname'
+		"tags.tagname AS tagname"
 	)
-	.innerJoin('poststags', 'tags.tagsid', 'poststags.tagsid')
-	.innerJoin('posts', 'poststags.postsid', 'posts.postsid')
-	.innerJoin('authors', 'posts.authorsid', 'authors.authorsid')
-	.groupBy('tags.tagname', 'authors.authorsid', 'authors.firstname', 'authors.lastname', 'authors.bio');
+	.innerJoin("poststags", "tags.tagsid", "poststags.tagsid")
+	.innerJoin("posts", "poststags.postsid", "posts.postsid")
+	.innerJoin("authors", "posts.authorsid", "authors.authorsid")
+	.groupBy("tags.tagname", "authors.authorsid", "authors.firstname", "authors.lastname", "authors.bio");
 };
 
 /*
@@ -65,34 +65,34 @@ function getAllAuthorsByAllTags(){
 */
 // get all posts per all tags
 function getAllPostsByAllTags(){
-	return db('tags')
+	return db("tags")
 	.select(
-		'tags.tagname AS tagname', 
-		'posts.postsid',
-		'authors.authorsid', 
+		"tags.tagname AS tagname", 
+		"posts.postsid",
+		"authors.authorsid", 
 		db.raw("authors.firstname || ' ' || authors.lastname AS author"),
-		'posts.postsid AS id',
-		'posts.likes AS likes', 
-		'posts.reads AS reads'
+		"posts.postsid AS id",
+		"posts.likes AS likes", 
+		"posts.reads AS reads"
 	)
-	.innerJoin('poststags', 'tags.tagsid', 'poststags.tagsid')
-	.innerJoin('posts', 'poststags.postsid', 'posts.postsid')
-	.innerJoin('authors', 'authors.authorsid', 'posts.authorsid');
+	.innerJoin("poststags", "tags.tagsid", "poststags.tagsid")
+	.innerJoin("posts", "poststags.postsid", "posts.postsid")
+	.innerJoin("authors", "authors.authorsid", "posts.authorsid");
 };
 
 /*
 	SELECT tags.tagsid, tags.tagname
 	FROM Tags
-	WHERE tags.tagname = 'tech'
+	WHERE tags.tagname = "tech"
 */
 // get tagsid & tagname on one tag
 function getOneTag(singleTagName){
-	return db('tags')
+	return db("tags")
 		.select(
-			'tags.tagname AS tagname', 
-			'tags.tagsid AS tagsid'
+			"tags.tagname AS tagname", 
+			"tags.tagsid AS tagsid"
 		)
-		.where('tags.tagname', singleTagName);
+		.where("tags.tagname", singleTagName);
 };
 
 /*
@@ -105,22 +105,22 @@ function getOneTag(singleTagName){
 	ON poststags.postsid = posts.postsid
 	INNER JOIN Authors
 	ON authors.authorsid = posts.authorsid
-	WHERE tags.tagname = 'tech'
+	WHERE tags.tagname = "tech"
 	GROUP BY tags.tagname, authors.authorsid, authors.firstname, authors.lastname, authors.bio;
 */
 // get all authors on one tag
 function getAllAuthorsByOneTag(tagName){
-	return db('tags')
+	return db("tags")
 	.select(
-		'authors.bio AS bio',
-		'authors.authorsid', 
+		"authors.bio AS bio",
+		"authors.authorsid", 
 		db.raw("authors.firstname || ' ' || authors.lastname AS author")
 	)
 	.distinct()
-	.innerJoin('poststags', 'tags.tagsid', 'poststags.tagsid')
-	.innerJoin('posts', 'poststags.postsid', 'posts.postsid')
-	.innerJoin('authors', 'posts.authorsid', 'authors.authorsid')
-	.where('tags.tagname', tagName);
+	.innerJoin("poststags", "tags.tagsid", "poststags.tagsid")
+	.innerJoin("posts", "poststags.postsid", "posts.postsid")
+	.innerJoin("authors", "posts.authorsid", "authors.authorsid")
+	.where("tags.tagname", tagName);
 };
 
 /*
@@ -130,23 +130,23 @@ function getAllAuthorsByOneTag(tagName){
 	ON tags.tagsid = poststags.tagsid
 	INNER JOIN Posts
 	ON poststags.postsid = posts.postsid
-	WHERE tags.tagname = 'tech'
+	WHERE tags.tagname = "tech"
 	GROUP BY tags.tagname, posts.postsid, posts.likes, posts.reads;
 */
 // get all posts on one tag
 function getAllPostsByOneTag(tagName){
-	return db('tags')
+	return db("tags")
 	.select(
-		'posts.postsid AS id', 
-		'posts.likes AS likes', 
-		'posts.reads AS reads', 
-		'authors.authorsid', 
+		"posts.postsid AS id", 
+		"posts.likes AS likes", 
+		"posts.reads AS reads", 
+		"authors.authorsid", 
 		db.raw("authors.firstname || ' ' || authors.lastname AS author")
 	)
-	.innerJoin('poststags', 'tags.tagsid', 'poststags.tagsid')
-	.innerJoin('posts', 'poststags.postsid', 'posts.postsid')
-	.innerJoin('authors', 'posts.authorsid', 'authors.authorsid')
-	.where('tags.tagname', tagName);
+	.innerJoin("poststags", "tags.tagsid", "poststags.tagsid")
+	.innerJoin("posts", "poststags.postsid", "posts.postsid")
+	.innerJoin("authors", "posts.authorsid", "authors.authorsid")
+	.where("tags.tagname", tagName);
 };
 
 /*
@@ -162,17 +162,17 @@ function getAllPostsByOneTag(tagName){
 */
 // get tags by author
 function getTagsByAuthor(authorsid) {
-	return db('tags')
+	return db("tags")
 		.select(
-			'authors.authorsid', 
-			db.raw('ARRAY_AGG(tags.tagname) AS tags')
+			"authors.authorsid", 
+			db.raw("ARRAY_AGG(tags.tagname) AS tags")
 		)
 		.distinct()
-		.innerJoin('poststags', 'posts.postsid', 'poststags.postsid')
-		.innerJoin('posts', 'authors.authorsid', 'posts.authorsid')
-		.innerJoin('authors', 'posts.authorsid', 'authors.authorsid')
-		.where('authors.authorsid', authorsid)
-		.groupBy('authors.authorsid');
+		.innerJoin("poststags", "posts.postsid", "poststags.postsid")
+		.innerJoin("posts", "authors.authorsid", "posts.authorsid")
+		.innerJoin("authors", "posts.authorsid", "authors.authorsid")
+		.where("authors.authorsid", authorsid)
+		.groupBy("authors.authorsid");
 };
 
 /*
@@ -186,15 +186,15 @@ function getTagsByAuthor(authorsid) {
 */
 // get tags by post id
 function getTagsByPost(postsid){
-	return db('tags')
+	return db("tags")
 		.select(
-			'posts.postsid', 
-			db.raw('ARRAY_AGG(tags.tagname) AS tags')
+			"posts.postsid", 
+			db.raw("ARRAY_AGG(tags.tagname) AS tags")
 		)
-		.innerJoin('poststags', 'poststags.tagsid', 'tags.tagsid')
-		.innerJoin('posts', 'poststags.postsid', 'posts.postsid')
-		.where('posts.postsid', postsid)
-		.groupBy('posts.postsid');
+		.innerJoin("poststags", "poststags.tagsid", "tags.tagsid")
+		.innerJoin("posts", "poststags.postsid", "posts.postsid")
+		.where("posts.postsid", postsid)
+		.groupBy("posts.postsid");
 };
 
 /*
@@ -208,47 +208,47 @@ function getTagsByPost(postsid){
 */
 // get tags
 function getTags(){
-	return db('tags')
+	return db("tags")
 		.select(
-			'posts.postsid AS postsid', 
-			db.raw('ARRAY_AGG(tags.tagname) AS tags')
+			"posts.postsid AS postsid", 
+			db.raw("ARRAY_AGG(tags.tagname) AS tags")
 		)
-		.innerJoin('poststags', 'poststags.tagsid', 'tags.tagsid')
-		.innerJoin('posts', 'poststags.postsid', 'poststags.postsid')
-		.groupBy('posts.postsid');
+		.innerJoin("poststags", "poststags.tagsid", "tags.tagsid")
+		.innerJoin("posts", "poststags.postsid", "poststags.postsid")
+		.groupBy("posts.postsid");
 };
 
 // find a tag by any field
 function findBy(filter) {
-	return db('tags')
+	return db("tags")
 		.where(filter);
 };
 
 // add a tag
 async function add(tag) {
-	const [tagsid] = await db('tags')
-		.insert(tag, 'tagsid');
+	const [tagsid] = await db("tags")
+		.insert(tag, "tagsid");
 	return findById(tagsid);
 };
 
 // find a tag by tag id
 function findById(tagsid) {
-	return db('tags')
-		.select('tagsid', '*')
+	return db("tags")
+		.select("tagsid", "*")
 		.where({ tagsid })
 		.first();
 };
 
 // update a tag by tag id
 function update(tagsid, tag) {
-	return db('tags')
+	return db("tags")
 		.where({ tagsid })
 		.update(tag);
 };
 
 // delete a tag by tag id
 function remove(tagsid) {
-	return db('tags')
-		.where('tagsid', Number(tagsid))
+	return db("tags")
+		.where("tagsid", Number(tagsid))
 		.del();
 };
