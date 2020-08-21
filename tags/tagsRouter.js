5ec935a759d7731854727f80d0d16203ac10f354
Erica L. Ingram <inquiries@aquoco.co>
2020-08-20 21:40:09 -0700

    fixed one tag's model

--- tags/tagsRouter.js
+++ tags/tagsRouter.js
@@ -389,11 +389,11 @@ router.get('/:tagname', restricted, (req, res) => {
 // GET:  get all authors per single tag
 	// /tags/<tag>/authors
 	// chain getOneTag, getAllAuthorsByOneTag
-router.get('/:tagname/authors', (req, res) => {
+router.get('/:tagname/authors', restricted, (req, res) => {
 	const singleTagName = req.params.tagname;
 	if (!singleTagName) {
 		res.status(404).json({
-			message: `The tag name ${tagName} does not exist.`,
+			message: `The tag name ${singleTagName} does not exist.`,
 			error: err
 		});
 	} else {
