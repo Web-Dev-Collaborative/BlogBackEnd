7f7f50c130a98e3e94f9de1d34837697610279d9
Erica L. Ingram <inquiries@aquoco.co>
2020-08-20 23:35:08 -0700

    fixing authors sort

--- authors/authorsHelpers.js
+++ authors/authorsHelpers.js
@@ -6,37 +6,38 @@ module.exports = {
 
 // authors endpoint fields:  bio, firstName, authorsid (id), lastName, posts, tags, totalLikeCount, totalReadCount
     // queries:  
-function compareFirst(a, b, sortField) {
+function compareFirst(a, b) {
     // ignore letter casing
-        const firstCompare = a.firstName.toUpperCase();
-        const secondCompare = b.firstName.toUpperCase();
-        let comparison = 0;
+    const firstCompare = a.firstName.toUpperCase();
+    const secondCompare = b.firstName.toUpperCase();
+    let comparison = 0;
 
-        if (firstCompare > secondCompare) {comparison = 1;} 
-        else if (firstCompare < secondCompare) {comparison = -1;}
+    if (firstCompare > secondCompare) {comparison = 1;} 
+    else if (firstCompare < secondCompare) {comparison = -1;}
 
-        return comparison;
+    return comparison;
 }
-function compareLast(a, b, sortField) {
+function compareLast(a, b) {
     // ignore letter casing
-        const firstCompare = a.lastName.toUpperCase();
-        const secondCompare = b.lastName.toUpperCase();
+    const firstCompare = a.lastName.toUpperCase();
+    const secondCompare = b.lastName.toUpperCase();
 
-        let comparison = 0;
+    let comparison = 0;
 
-        if (firstCompare > secondCompare) {comparison = 1;} 
-        else if (firstCompare < secondCompare) {comparison = -1;}
+    if (firstCompare > secondCompare) {comparison = 1;} 
+    else if (firstCompare < secondCompare) {comparison = -1;}
 
-        return comparison;
+    return comparison;
 }
-function compareID(a, b, sortField) {
+function compareID(a, b) {
     // ignore letter casing
-        const firstCompare = a.id;
-        const secondCompare = b.id;
-        let comparison = 0;
+    const firstCompare = a.id;
+    const secondCompare = b.id;
 
-        if (firstCompare > secondCompare) {comparison = 1;} 
-        else if (firstCompare < secondCompare) {comparison = -1;}
+    let comparison = 0;
 
-        return comparison;
+    if (firstCompare > secondCompare) {comparison = 1;} 
+    else if (firstCompare < secondCompare) {comparison = -1;}
+
+    return comparison;
 }
